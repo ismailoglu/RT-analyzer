a1d3df9b317af25ef293df12139f38ad5ab1942b
slacktracer <slacktracer@gmail.com>
2015-11-06 15:25:15 -0300

    A new controller in town. GenLoad helper is in the experiment module now.

--- public/modules/experiment/modules/processor/controllers/processor.controller.js
+++ public/modules/experiment/modules/processor/controllers/processor.controller.js
@@ -17,50 +17,8 @@
             vm;
         vm = this;
         vm.experiment = experiment;
-        // vm.data = experiment.data;
         vm.intoCSVList = intoCSVList;
-        // vm.metadata = experiment.metadata;
         vm.processData = processor.processData;
-        // console.log(experiment)
-        experiment.raw = JSON.stringify(
-            experiment,
-            function replacer(key, value) {
-                if (key === "raw") {
-                    return undefined;
-                }
-                return value;
-            }
-        );
-        // vm.allData = experiment.raw;
-        vm.load = function load() {
-            var reload = JSON.parse(vm.allData);
-            reload.metadata.biologicalReplicatesGroups.add = experiment.metadata.biologicalReplicatesGroups.add;
-            reload.metadata.biologicalReplicatesGroups.remove = experiment.metadata.biologicalReplicatesGroups.remove;
-            reload.metadata.helpers.coerceModel = experiment.metadata.helpers.coerceModel;
-            reload.metadata.probes.add = experiment.metadata.probes.add;
-            reload.metadata.probes.change = experiment.metadata.probes.change;
-            reload.metadata.probes.remove = experiment.metadata.probes.remove;
-            reload.metadata.samples.add = experiment.metadata.samples.add;
-            reload.metadata.samples.change = experiment.metadata.samples.change;
-            reload.metadata.samples.remove = experiment.metadata.samples.remove;
-            reload.metadata.biologicalReplicatesGroups.add = experiment.metadata.biologicalReplicatesGroups.add;
-            reload.metadata.biologicalReplicatesGroups.change = experiment.metadata.biologicalReplicatesGroups.change;
-            reload.metadata.biologicalReplicatesGroups.remove = experiment.metadata.biologicalReplicatesGroups.remove;
-            experiment.data = reload.data;
-            experiment.metadata = reload.metadata;
-        }
-        vm.gen = function gen() {
-            // experiment.raw = JSON.stringify(
-            //     experiment,
-            //     function replacer(key, value) {
-            //         if (key === "raw") {
-            //             return undefined;
-            //         }
-            //         return value;
-            //     }
-            // );
-            vm.allData = experiment.raw;
-        }
         /*
          * functions
          */
